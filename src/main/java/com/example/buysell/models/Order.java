18c5c5a86d71f393fd51b359e7a65b22d8bc235d
Lenarin <Vladislavakia425@gmail.com>
2022-06-27 08:02:11 +0300

    Модификация 1 - добавление модели заказа

---
+++ src/main/java/com/example/buysell/models/Order.java
@@ -0,0 +1,36 @@
+package com.example.buysell.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User seller;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User buyer;
+    @Column(name = "destination")
+    private String destination;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+}
